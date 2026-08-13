c2690055765a4dd258d9be24fcb94efd349998d9
emiyablake <maju.ads.02@gmail.com>
2026-08-12 21:16:39 -0300

    update project dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-module.exports = {
-  preset: 'ts-jest',
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
